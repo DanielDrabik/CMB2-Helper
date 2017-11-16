e6651d2686662ac99f2a423395ed8e99db29bdeb
Daniel Drabik <daniel.drabik94@gmail.com>
2017-11-16 12:48:38 +0100

    Create cmb_option.php

---
+++ cmbHelper/cmb_option.php
@@ -0,0 +1,141 @@
+<?php
+
+class cmbOption {
+
+	public $prefix;
+	public $option_key;
+	public $title;
+
+	private $metabox;
+
+	private $tabsSetting;
+	private $currentTab = null;
+	private $currentTabPrefix;
+
+	private $currentField = null;
+
+	public function __construct($option_key, $title, $icon = false) {
+
+		$this->option_key = $option_key;
+		$this->title = $title;
+
+		$this->metabox = array(
+			'id'          => $this->option_key . '_metabox',
+			'title'       => __( $this->title, 'cmb2' ),
+			'show_names'  => true,
+			'object_type' => 'options-page',
+			'show_on'     => array(
+				// These are important, don't remove
+				'key'   => 'options-page',
+				'value' => array( $this->option_key )
+			),
+		);
+
+		$this->tabsSetting  = array(
+	        'config' => $this->metabox,
+	        'layout' => 'vertical', // Default : horizontal
+	        'tabs'   => array()
+	    );
+
+	    if($icon)
+	    	$this->metabox['icon_url'] = $icon;
+	}
+
+	public function addTab($prefix, $title) {
+
+		if($this->currentField != null) {
+			$this->currentTab['fields'][] = $this->currentField;
+			$this->currentField = null;
+		}				
+
+		if($this->currentTab != null) 
+			$this->tabsSetting['tabs'][] = $this->currentTab;
+
+		$this->currentTabPrefix = $prefix;
+		$this->currentTab = array(
+	        'id'     => $prefix,
+	        'title'  => __( $title, 'cmb2' ),
+	        'fields' => array()
+	    );
+	}
+
+	public function addField($id, $name, $type, $group_id = false, $options = false, $default = false) {
+
+		if($group_id) {
+
+			if(!$options)
+			$this->currentField['fields'][] =  array(
+			        'name' => __( $name, 'cmb2' ),
+			        'id'   => $id,
+			        'type' => $type,
+			    );	
+			else 		
+				$this->currentField['fields'][] =  array(
+			        'name' => __( $name, 'cmb2' ),
+			        'id'   => $id,
+			        'type' => $type,
+			        'options' => $options,
+			    );	
+
+			return true;		
+		}
+		else {
+
+			if($this->currentField != null)
+				$this->currentTab['fields'][] = $this->currentField;
+
+			
+			if($type == 'group') 
+			
+				$this->currentField =  array(
+			        'name' => __( $name, 'cmb2' ),
+			        'id'   => $this->currentTabPrefix .'_'. $id,
+			        'type' => $type,
+			        'options' => array(
+			            'group_title' => __('Element {#}', 'cmb2'), // since version 1.1.4, {#} gets replaced by row number			            
+			            'sortable' => true, // beta
+			            'closed' => true, // true to have the groups closed by default
+			        ),
+			    );
+			
+			else 
+
+				$this->currentField =  array(
+			        'name' => __( $name, 'cmb2' ),
+			        'id'   => $this->currentTabPrefix .'_'. $id,
+			        'type' => $type,
+			    );
+			
+		}
+
+		if($options) 
+			$this->currentField['options'] = $options;
+		
+		if($default !== false) 
+			$this->currentField['default'] = $default;
+
+	}
+
+	public function generateCMB() {
+
+		if($this->currentField != null)
+				$this->currentTab['fields'][] = $this->currentField;
+
+		if($this->currentTab != null) 
+			$this->tabsSetting['tabs'][] = $this->currentTab;
+
+		return $this->metabox;
+	}
+
+	public function generateTabs() {
+
+		$tabs = array(
+	        'id'   => $this->option_key . '__tabs',
+	        'type' => 'tabs',
+	        'tabs' => $this->tabsSetting,
+	    );
+
+	    return $tabs;
+	}
+
+}
